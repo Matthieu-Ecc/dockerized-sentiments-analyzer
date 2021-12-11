fbc8f4221e9d4245954cb6c2ba15c7f1173f7851
Rayan Edjekouane <rayan.edjekouane@efrei.net>
2021-12-11 20:22:07 +0100

    Web cont : Post form

--- docker_web/app.py
+++ docker_web/app.py
@@ -3,23 +3,15 @@ from flask import Flask, request, render_template
 app = Flask(__name__)
 
 @app.route('/')
-def my_form():
+def form():
     return render_template('index.html')
 
 
-@app.route("/askSentiments", methods = ['POST','GET'])
-def askSentiments():
-    if request.method == 'POST':
-        student_name = request.form['student_name']
-        website = request.form.get('website')
-        return 'Submitted!'
-        return '''<form method = "post">
-        <p>Enter Name:</p>
-        <p><input type = "text" name = "studentName" /></p>
-        <p>Enter Website:</p>
-        <p><input type = "text" name = "website" /></p>
-        <p><input type = "submit" value = "submit" /></p>
-        </form>'''
+@app.route('/', methods=['POST'])
+def analyse():
+    text_to_analyse = request.form['text']
+    #final = Appeler l'autre docker pour inferer
+    return text_to_analyse
 
 
 if __name__ == "__main__":
