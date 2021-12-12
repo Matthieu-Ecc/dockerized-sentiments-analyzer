9dd8a1d033adc5997db039f1398e487ee03d0067
Rayan Edjekouane <rayan.edjekouane@efrei.net>
2021-12-12 12:51:04 +0100

    Clean logs and docker compose of web cont

--- docker_ml/vader_API.py
+++ docker_ml/vader_API.py
@@ -7,9 +7,6 @@ api = Api(app)
 class HelloWorld(Resource):
     def get(self):
         sentence = request.args.get("data")
-        f  = open("logs.txt","w")
-        f.write(sentence)
-        f.close
         return {'sentence': sentence}
 
 api.add_resource(HelloWorld, '/')
