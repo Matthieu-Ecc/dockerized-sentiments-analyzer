f5b3e62ab260dd7b88f6ae293ce3f335524cbb15
matthieu <matthieu.eccher@free.fr>
2021-12-14 10:45:14 +0100

    stress test

--- test_app.py
+++ test_app.py
@@ -35,3 +35,12 @@ def test_sentiment():
 
 def test_connection():
     assert requests.get("http://localhost:5000").status_code == 200 , "web site is not up"
+
+def test_stress_requests():
+
+    start = time.time()
+    for i in range(1000):
+        requests.get("http://localhost:5000")
+
+    end = time.time()-start
+    assert (end/1000) < 100 ,"stress not passed"
